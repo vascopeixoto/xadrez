834befaaf892ac847133d1c6141633eebc8ba19e
vascopeixoto <vascomfpeixoto@gmail.com>
2022-03-10 17:26:49 +0000

    peças

--- xadrez_console/Tabuleiro/Peca.cs
+++ xadrez_console/Tabuleiro/Peca.cs
@@ -8,11 +8,11 @@ namespace tabuleiro
         public int qteMov { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
-            this.cor = cor;
+            posicao = null;
             this.tab = tab;
+            this.cor = cor;
             qteMov = 0;
         }
 
