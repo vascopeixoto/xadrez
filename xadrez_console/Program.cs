5fb63ba7791fe6eef675de9b5940993e6dddfa30
vascopeixoto <vascomfpeixoto@gmail.com>
2022-03-11 10:15:28 +0000

    Imprimir peca

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,9 +8,26 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.ToPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(2, 4));
+
+
+
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
             Console.ReadLine();
 
         }
--- xadrez_console/Tela.cs
+++ xadrez_console/Tela.cs
@@ -4,19 +4,50 @@ namespace xadrez_console
 {
     class Tela
     {
+
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
+            ConsoleColor aux = Console.ForegroundColor;
+
             for (int i = 0; i < tab.linhas; i++)
             {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(8 - i + " " + " " + "|  ");
+                Console.ForegroundColor = aux;
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(tab.peca(i, j) + " ");
+                    {
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
             }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    -----------------");
+            Console.WriteLine("      a b c d e f g h");
+            Console.ForegroundColor = aux;
         }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+
+            if (peca.cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+
     }
 }
